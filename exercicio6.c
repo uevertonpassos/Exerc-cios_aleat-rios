0448557fece4a4f6e9c656e0479e53256b2bacfc
Ueverton Passos <uevertonpassos96@gmail.com>
2023-02-26 22:52:24 -0300

    exercicios while

---
+++ exercicio6.c
@@ -0,0 +1,14 @@
+/*Escreva um programa em C
+ que imprima os números de 0 a 9 
+ usando um loop while.*/
+
+ #include <stdio.h>
+
+ void main(){
+    int i = 0;
+    
+    while(i < 10){
+        printf("%d ",i);
+        i++;
+    }
+ }
